487ac4301e46e2a2e50b5851067f26b2ea8b0b85
Marion <marion_ott@me.com>
2020-03-10 11:20:14 +0100

    update database connection info

--- server/server.js
+++ server/server.js
@@ -1,26 +1,38 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
-const DB = "mongodb://mongodb:27017/users"
+dotenv.config({
+  path: './config.env'
+})
+
+// const DB = 'mongodb://mongodb:27017/users'
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+)
 const PORT = 9000
 
 // Connect to database
-mongoose.connect(DB, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
-}).then(() => {
-  console.log("Connected to DB");
-}).catch((e) => {
-  console.log("Error while connecting to DB")
-  console.error(e)
-})
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => {
+    console.log('Connected to DB')
+  })
+  .catch(e => {
+    console.log('Error while connecting to DB')
+    console.error(e)
+  })
 
 // Define listening port
-const app = require('./app');
-const server = app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+const app = require('./app')
+const server = app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
 
 // Close server if an error is caught
 process.on('unhandledRejection', err => {
-  console.error(err.name, err.message);
-  server.close(() => process.exit(1));
-});
+  console.error(err.name, err.message)
+  server.close(() => process.exit(1))
+})
